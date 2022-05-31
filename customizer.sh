856cf38435c817e21a1f2bd64111f7f1b44160f3
subje2 <sdbb2q@gmail.com>
2022-05-31 20:43:20 +0900

    Add files via upload

---
+++ customizer.sh
@@ -0,0 +1,10 @@
+//Copyright sujeb2 2021-2022
+//ONLY GNOME DESKTOP!!! NOT 'kde, i3, xface, wayland, etc'!!
+echo "[ prototype ]"
+sudo apt update
+sudo apt install gnome-tweaks
+cd /home/
+mkdir .themes
+mkdir .icons
+echo "installation complete"
+gnome-tweaks
